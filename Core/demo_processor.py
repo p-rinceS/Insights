b5d6668c032eb49a6c151e4f2a9c158862570a22
Prince <princesonani9112@gmail.com>
2025-06-28 20:00:40 -0500

    add leaderboard every round

--- Core/demo_processor.py
+++ Core/demo_processor.py
@@ -1,3 +1,5 @@
+from traceback import print_exc
+import concurrent.futures
 import pandas as pd
 from demoparser2 import DemoParser
 import os
@@ -17,37 +19,32 @@ def clear_voices_directory():
     else:
         print(f"Demo folder {voices_dir} does not exist.")
 
+
+def process_demo(demo_path):
+    try:
+        parser = DemoParser(demo_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Demo file not found: {demo_path}")
+    return parser
+
+def extract_team_data(players, team_number):
+    return players[players['team_number'] == team_number].apply(
+        lambda row: {"name": row['name'], "steamid": row['steamid']}, axis=1
+    ).tolist()
+
 class DemoProcessor:
     def __init__(self, demo_path):
         self.demo_path = demo_path
         self.demo_id = os.path.splitext(os.path.basename(demo_path))[0]
 
-    def process_demo(self, demo_path):
-        try:
-            parser = DemoParser(demo_path)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Demo file not found: {demo_path}")
-        return parser
-
-    def parse_voice(self):
-        return self.parser.parse_voice()
-
-    def parse_player_info(self):
-        return self.parser.parse_player_info()
-    
-    def extract_team_data(self, players, team_number):
-        return players[players['team_number'] == team_number].apply(
-            lambda row: {"name": row['name'], "steamid": row['steamid']}, axis=1
-        ).tolist()
-
     def get_player_teams(self):
         try:
-            parser = self.process_demo(self.demo_path)
+            parser = process_demo(self.demo_path)
             parser.parse_header()
             players = parser.parse_player_info()
             teams = players['team_number'].unique()
-            team1 = self.extract_team_data(players, teams[0])
-            team2 = self.extract_team_data(players, teams[1])
+            team1 = extract_team_data(players, teams[0])
+            team2 = extract_team_data(players, teams[1])
             return team1, team2
         except Exception as e:
             print(f"An error occurred while getting player teams: {e}")
@@ -59,7 +56,7 @@ class DemoProcessor:
         except ValueError:
             print(f"Invalid Steam ID: {steam_id}. Steam ID must be an integer.")
         try:
-            parser = self.process_demo(self.demo_path)
+            parser = process_demo(self.demo_path)
             parser.parse_header()
             players = parser.parse_player_info()
             player_row = players.loc[players['steamid'] == steam_id]
@@ -68,9 +65,22 @@ class DemoProcessor:
             print(f"An error occurred while retrieving player name: {e}")
             return "Unknown Player"
 
+    def get_round_end_ticks(self):
+        try:
+            parser = process_demo(self.demo_path)
+            parser.parse_header()
+            game_end_ticks = parser.parse_event("round_end")["tick"]
+            # Return a list of round end ticks
+            tick_list = list(game_end_ticks)
+            print(f"Round end ticks: {tick_list}")
+            return tick_list
+        except Exception as e:
+            print(f"An error occurred while getting round end ticks: {e}")
+            return []
+
     def process_voices_by_team(self):
         try:
-            parser = self.process_demo(self.demo_path)
+            parser = process_demo(self.demo_path)
             parser.parse_header()
             streamed_bytes = parser.parse_voice()
             team1, team2 = self.get_player_teams()
@@ -98,23 +108,39 @@ class DemoProcessor:
     def set_demo_id(self, demo_id):
         self.demo_id = demo_id
 
+    def get_start_time(self):
+        try:
+            parser = process_demo(self.demo_path)
+            parser.parse_header()
+            return parser.parse_event("round_start").get("time", None)
+        except Exception as e:
+            print(f"An error occurred while getting start time: {e}")
+            return None
 
+
+    def get_game_leaderboard_every_round(self, round):
+        try:
+            parser = process_demo(self.demo_path)
+            parser.parse_header()
+            game_events = parser.list_game_events()
+            print(f"Game events: {game_events}")
+            round_end_ticks = self.get_round_end_ticks()
+            print(f"Game end tick: {round_end_ticks[round]}")
+            wanted_fields = ["kills_total", "deaths_total","assists_total",  "mvps"]
+            df = parser.parse_ticks(wanted_fields, ticks=[round_end_ticks[round]])
+            print(df)
+        except Exception as e:
+            print(f"An error occurred while getting player positions at round {round + 1}: {e}")
+            return None
+    def get_player_stats(self, tick):
+        raise NotImplementedError("get_player_stats method is not implemented yet.")
 
 
 if __name__ == "__main__":
     print("Demos processing...")
-    dp = DemoProcessor("../assets/demos/inferno.dem")
+    dp = DemoProcessor("../assets/demos/nuke.dem")
     seed = 0
     rd = random.Random()
     rd.seed(seed)
-
     dp.set_demo_id(uuid.UUID(int=rd.getrandbits(128)))
-    dp.process_voices_by_team()
-
-    dp2 = DemoProcessor("../assets/demos/mirage.dem")
-    seed = 2
-    rd = random.Random()
-    rd.seed(seed)
-    dp2.set_demo_id(uuid.UUID(int=rd.getrandbits(128)))
-    dp2.process_voices_by_team()
-    print("Demos processed successfully.")
+    dp.get_game_leaderboard_every_round(1)
